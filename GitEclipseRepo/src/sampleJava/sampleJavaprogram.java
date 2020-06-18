e147df424b1408012a31d614edbaf514b026743b
VisalamMurugan <visalam.murugan@gmail.com>
2020-06-18 16:34:05 +0530

    first commit is donw

---
+++ GitEclipseRepo/src/sampleJava/sampleJavaprogram.java
@@ -0,0 +1,10 @@
+package sampleJava;
+
+public class sampleJavaprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to sampleJavaProgram");
+	}
+
+}
